f1a2d29e3895ad35f5623361fdf94d6b19657760
Martina <795809@unizar.es>
2023-03-07 20:24:00 +0100

    fichero ruby ping + ssh (no va )

--- u.rb
+++ u.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 
 require 'net/ping'
+require 'net/ssh'
 # u subcomando opciones
 
 file_data = File.read("./hosts").split
 # ["host1", "host22", "user3"]
 
-if ARGV[0] == p
+if ARGV[0] == 'p'
     file_data.each do |host|
         pingable = Net::Ping::External.new(host)
         if pingable.ping?
@@ -17,6 +18,18 @@ if ARGV[0] == p
         puts "Maquina  #{host}  : #{v}"
 
     end
+elsif ARGV[0] == 's'
+    file_data.each do |host|
+            Net::SSH.start(host, "a795809") do |ssh|
+            result = ssh.exec!("ls -l")
+
+            if result != null
+                v = "FUNCIONA"
+            else
+                v = "falla"
+            end
+            puts "Maquina  #{host}  : #{v}"
+            puts result
+        end
+    end
 end
-
-
