a50831c64f74d571271601b1cad20a71c96590e8
Martina <795809@unizar.es>
2023-03-28 22:26:18 +0200

    practica 3

--- encender.sh
+++ encender.sh
@@ -5,3 +5,5 @@ sleep 2
 virsh -c qemu+ssh://a795809@155.210.154.204/system start o8ff3
 sleep 2
 virsh -c qemu+ssh://a795809@155.210.154.204/system start o8ff4
+sleep 2
+virsh -c qemu+ssh://a795809@155.210.154.204/system start o8router81
